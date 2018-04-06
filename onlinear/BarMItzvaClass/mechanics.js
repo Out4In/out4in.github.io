8006f8c826846e2c7556180f6cf66a36db7ab67f
Unknown <out4in.developer@gmail.com>
2018-04-06 11:31:41 +0300

    Update Lib to work with ios11

--- onlinear/BarMItzvaClass/mechanics.js
+++ onlinear/BarMItzvaClass/mechanics.js
@@ -1,18 +1,22 @@
 var ALL_IMAGES = []
 var DETECTED_IMAGES = []
+var TD_ELEMENTS = []
 var PARTS = 8
 for(var i = 1; i <= PARTS; i++)
 {
     var tmp_img = document.createElement("img");
     tmp_img.src = "images/img_" + i  +".png";
     ALL_IMAGES.push(tmp_img)
+    
+    
+    var tmp_td = document.getElementById("img" + i);
+    TD_ELEMENTS.push(tmp_td);
 }
 
 
 
 function AddImageAsDetected(image_name)
 {
-    debugger;
     var index = 0;
     for(; index < PARTS;index++) // check if image in all images
     {
@@ -35,4 +39,35 @@ function AddImageAsDetected(image_name)
     }
     
     DETECTED_IMAGES.push(ALL_IMAGES[index]);
+    var attach_img = false;
+    while(!attach_img)
+    {
+        var r_number = Math.floor(Math.random()*8 + 1);
+        if(!TD_ELEMENTS[r_number - 1].hasChildNodes())
+        {
+            TD_ELEMENTS[r_number - 1].appendChild(ALL_IMAGES[index]);
+            attach_img = true;
+        }
+    }   
+    
+}
+
+function MoveToPuzzle()
+{
+    var elm = document.getElementById('camera_holder');
+    elm.style.visibility = "hidden";
+    elm.style.display = "none";
+    elm = document.getElementById("puzzle_holder");
+    elm.style.visibility = "visible";
+    elm.style.display = "block";
+}
+
+function MoveToCamera()
+{
+    var elm = document.getElementById('puzzle_holder');
+    elm.style.visibility = "hidden";
+    elm.style.display = "none";
+    elm = document.getElementById("camera_holder");
+    elm.style.visibility = "visible";
+    elm.style.display = "block";
 }
